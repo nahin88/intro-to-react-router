a0fcc936a53a5bea0978ab568899027d47977c83
nahin88 <nahiraja88@gmail.com>
2023-04-06 03:05:02 +0600

    Link,loader,useLoaderData,module 54-3

---
+++ src/components/First/First.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const First = () => {
+    return (
+        <div>
+            <h3>This is the default web sight Page.</h3>
+        </div>
+    );
+};
+
+export default First;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
-import Home from './components/Home/Home';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Home from "./components/Home/Home";
+import First from "./components/First/First";
+import Friends from "./components/Friends/Friends";
 
 // const router = createBrowserRouter([
 //   {
@@ -28,23 +26,32 @@ import Home from './components/Home/Home';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home></Home>,
     children: [
       {
-        path: '/about',
-        element: <About></About>
+        path: "/",
+        element: <First></First>,
       },
       {
-        path: '/contact',
-        element: <Contact></Contact>
-      }
-    ]
-  }
-])
+        path: "/friends",
+        element: <Friends></Friends>,
+        loader: () => fetch("https://jsonplaceholder.typicode.com/users"),
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
