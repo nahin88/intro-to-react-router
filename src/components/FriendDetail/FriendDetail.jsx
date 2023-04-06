83ab02909ef5c1fa6d7e7e1596b67f765c503210
nahin88 <nahiraja88@gmail.com>
2023-04-06 22:43:26 +0600

    create dynamic link with route parameter

---
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendDetail = () => {
+    return (
+        <div>
+            <h3>Everything about this person in detail is here.</h3>
+        </div>
+    );
+};
+
+export default FriendDetail;
--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import {useLoaderData} from "react-router-dom";
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
-    const data = useLoaderData();
-    console.log(data)
+    const friends = useLoaderData();
+    // console.log(friends)
     return (
         <div>
-            <h3>These are my friend jsx component.</h3>
-        </div>
+            <h3>These are my friends: {friends.length}</h3>
+            <div>
+                {
+                    friends.map(friend => <Friend
+                        key={friend.id}
+                        friend={friend}
+                    ></Friend>)
+                }
+            </div>
+        </div> 
     );
 };
 
